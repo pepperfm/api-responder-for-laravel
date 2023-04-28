16b137d19e72cf53c74ace2a156bce520c6a2615
PepperFM <Damon3453@yandex.rui>
2023-04-29 01:58:38 +0300

    [fix] docblock facade for phpstorm highlighting

--- src/BaseResponse.php
+++ src/BaseResponse.php
@@ -5,10 +5,10 @@ namespace Pepperfm\ApiBaseResponder;
 use Illuminate\Support\Facades\Facade;
 
 /**
- * @method static \Illuminate\Http\JsonResponse response(array $data, string $message = 'Success', int $httpStatusCode = \Illuminate\Http\JsonResponse::HTTP_OK)
- * @method static \Illuminate\Http\JsonResponse error(string $message = 'Error', int $httpStatusCode = \Illuminate\Http\JsonResponse::HTTP_INTERNAL_SERVER_ERROR, mixed $errors = null, mixed $data = null)
- * @method static \Illuminate\Http\JsonResponse stored(array $data, string $message = '')
- * @method static \Illuminate\Http\JsonResponse deleted(array $data, string $message = '')
+ * @method \Illuminate\Http\JsonResponse response(array $data, string $message = '', int $httpStatusCode = 200)
+ * @method \Illuminate\Http\JsonResponse error(string $message = '', int $httpStatusCode = 500, mixed $errors = null, mixed $data = null)
+ * @method \Illuminate\Http\JsonResponse stored(array $data, string $message = '')
+ * @method \Illuminate\Http\JsonResponse deleted(array $data, string $message = '')
  *
  * @see \Pepperfm\ApiBaseResponder\ApiBaseResponder
  */
