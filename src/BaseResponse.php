6838351ebb3640787eb6b7e08cb3288dc3f0bbc3
PepperFM <Damon3453@yandex.ru>
2023-04-29 19:33:12 +0300

    [fix] rollback facade phpdoc

--- src/BaseResponse.php
+++ src/BaseResponse.php
@@ -5,8 +5,8 @@ namespace Pepperfm\ApiBaseResponder;
 use Illuminate\Support\Facades\Facade;
 
 /**
- * @method \Illuminate\Http\JsonResponse response(array $data, string $message = '', int $httpStatusCode = 200)
- * @method \Illuminate\Http\JsonResponse error(string $message = '', int $httpStatusCode = 500, mixed $errors = null, mixed $data = null)
+ * @method \Illuminate\Http\JsonResponse response(array $data, string $message = 'Success', int $httpStatusCode = \Illuminate\Http\JsonResponse::HTTP_OK)
+ * @method \Illuminate\Http\JsonResponse error(string $message = 'Error', int $httpStatusCode = \Illuminate\Http\JsonResponse::HTTP_INTERNAL_SERVER_ERROR, mixed $errors = null, mixed $data = null)
  * @method \Illuminate\Http\JsonResponse stored(array $data, string $message = '')
  * @method \Illuminate\Http\JsonResponse deleted(array $data, string $message = '')
  *
